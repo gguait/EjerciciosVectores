ad328f015bea232702e2795f720d07336501936f
Pablo <Pablo@Revision-PC>
2022-12-02 22:23:37 +0100

    Ejercicio 1 hecho

--- src/vectores/Vectores1.java
+++ src/vectores/Vectores1.java
@@ -1,15 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package vectores;
+
+import java.util.Scanner;
 
 /**
  *
  * @author Pablo
  */
 public class Vectores1 {
+    //  Escribe un programa que permita introducir los valores de un vector de
+    //  diez elementos numéricos y luego imprimirlos.
     public static void main(String[] args) {
-        System.out.println("hola");
+        Scanner teclado = new Scanner(System.in);
+        int[] numeros = new int[10];
+        
+        System.out.println("Introduce diez valores");
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = teclado.nextInt();
+        }
+        
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i]+" ");
+        }
+        
     }
 }
