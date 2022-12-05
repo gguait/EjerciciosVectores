907080788a9f7392ff34f1631cfef6670a39dac7
Pablo Uraga Martinez <emptywhitelist@gmail.com>
2022-12-05 16:55:40 +0100

    Ejercicio 11 WIP

---
+++ src/vectores/Ejercicio11.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vectores;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pabmar
+ */
+public class Ejercicio11 {
+//        Escribe un programa que permita introducir un vector P de diez
+//        elementos numéricos, visualizar su contenido, crear un vector V con
+//        los elementos de P que sean mayores de diez. Visualizar luego el
+//        contenido de V.
+    
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        
+        
+        int[] vectorP = {0,1,2,3,12,5,6,142,8,9};
+        int contadorMayoresDiez = 0;
+//        int[] vectorP = new int[10];
+//        
+//        System.out.println("Introduce 10 elementos numericos");
+//        
+//        for (int i = 0; i < vectorP.length; i++) {
+//            vectorP[i] = teclado.nextInt();
+//        }
+        for (int i = 0; i < vectorP.length; i++) {
+            if (vectorP[i] >= 10) {
+                contadorMayoresDiez++;
+            }
+            System.out.println(vectorP[i]);
+        }
+        System.out.println("");
+            
+        int[] vectorV = new int [contadorMayoresDiez];
+        
+        for (int i = 0; i < vectorP.length; i++) {
+            if (vectorP[i] >=10) {
+                int j=0;
+                vectorV[j] = vectorP[i];
+                j++;
+            }
+        }
+        for (int i = 0; i < vectorV.length; i++) {
+            System.out.println(vectorV[i]);
+        }
+    }
+}
