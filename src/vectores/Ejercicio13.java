a444a790664b856fe8e28d41f1ab83648ff9f299
Pablo Uraga Martinez <emptywhitelist@gmail.com>
2022-12-12 16:20:48 +0100

    Ejercicio 12 DONE + 13 WIP

---
+++ src/vectores/Ejercicio13.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vectores;
+
+/**
+ *
+ * @author pabmar
+ */
+public class Ejercicio13 {
+//    Escribe un programa que permita, partiendo de un Vector V de
+//    cincuenta elementos, generar un vector P de cincuenta elementos de
+//    forma que: 
+
+    public static void main(String[] args) {
+        int[] vector = new int[50];
+        
+        int aux = 1;
+        
+        
+        for (int i = 0; i < vector.length; i++) {
+            
+        }
+    }
+}
